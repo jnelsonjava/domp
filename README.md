0dde34d83606def7d87deac1ee17638468373810
Jack Nelson <jnelson.java@gmail.com>
2020-10-29 23:11:13 -0700

    accessing hardcoded file URI and playing

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 Music player focusing on playlists
 
+## Useful Resources
+
+  - [Android Pedia](https://androidpedia.net/)
+  - [Android-Code Blogspot](https://android--code.blogspot.com/)
+
--- app/src/main/java/com/jnelsonjava/domp/MainActivity.java
+++ app/src/main/java/com/jnelsonjava/domp/MainActivity.java
@@ -1,32 +1,70 @@
 package com.jnelsonjava.domp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 
-import android.content.ContentResolver;
-import android.content.ContentUris;
-import android.database.Cursor;
-import android.media.AudioAttributes;
-import android.media.AudioManager;
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-import java.io.IOException;
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    private Context context;
+    private Activity activity;
+
+    private ConstraintLayout rootLayout;
+    private Button triggerMusicButton;
+
+    private MediaPlayer mediaPlayer;
+
+    private static final int MY_CODE = 456;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.hm);
-        mediaPlayer.start();
+        context = getApplicationContext();
+        activity = MainActivity.this;
+
+        rootLayout = findViewById(R.id.main_activity_layout);
+
+        triggerMusicButton = findViewById(R.id.music_trigger_button);
+        triggerMusicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    System.out.println("entering check permission");
+                    checkPermission();
+                } else {
+                    playMusic();
+                }
+            }
+        });
+
+//        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.hm);
+//        mediaPlayer.start();
 
         //-----------------------------------------------------
 
-        System.out.println(MediaStore.Audio.Media.getContentUri(""));
+//        System.out.println(MediaStore.Audio.Media.getContentUri(""));
 
 
 //        ContentResolver contentResolver = getContentResolver();
@@ -69,5 +107,67 @@ public class MainActivity extends AppCompatActivity {
 
         //------------------------------------------------------------
 
+    }
+
+    protected void playMusic() {
+        File file = new File("/storage/emulated/0/Music/OrangeForge/09 Semi-Permanent Makeup.mp3");
+        System.out.println(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC));
+//        File file1 = Environment.getExternalStorageDirectory();
+        System.out.println("in play music!!!!");
+        if (file.exists()) {
+            Uri uri = Uri.fromFile(file);
+
+            mediaPlayer = MediaPlayer.create(context, uri);
+
+            mediaPlayer.start();;
+
+            Toast.makeText(context, "Music playing", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+        System.out.println(PackageManager.PERMISSION_GRANTED);
+        System.out.println(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED);
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+                    builder.setMessage("Need permission to read external storage");
+                    builder.setTitle("Please give permission");
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(
+                                    activity,
+                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                    MY_CODE
+                            );
+                        }
+                    });
+                    builder.setNeutralButton("Cancel", null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                } else {
+                    ActivityCompat.requestPermissions(
+                            activity,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            MY_CODE
+                    );
+                }
+            } else {
+                System.out.println("this should go to play music");
+                playMusic();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == MY_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                playMusic();
+            }
+        }
     }
 }
